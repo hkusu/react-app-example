61b71f535ab2c4208f2b43f624222a5cb62d6994
hkusu <kusu0806@gmail.com>
2016-09-21 00:12:11 +0900

    Fix indent

--- src/component/ItemList.js
+++ src/component/ItemList.js
@@ -18,18 +18,18 @@ const ItemList = (props: {
     }
     <table>
       <tbody>
-      {
-        props.items.map((item: ItemType): React.Element<> => (
-          <Item
-            key={item.uuid}
-            userProfileImageUrl={item.user.profile_image_url}
-            title={item.title}
-            url={item.url}
-            updatedAdInWords={item.updated_at_in_words}
-            stockCount={item.stock_count}
-          />
-        ))
-      }
+        {
+          props.items.map((item: ItemType): React.Element<> => (
+            <Item
+              key={item.uuid}
+              userProfileImageUrl={item.user.profile_image_url}
+              title={item.title}
+              url={item.url}
+              updatedAdInWords={item.updated_at_in_words}
+              stockCount={item.stock_count}
+            />
+          ))
+        }
       </tbody>
     </table>
   </div>
