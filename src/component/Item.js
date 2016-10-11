b473c74d2ddbde42afd726641cb2f2b7b4662e86
hkusu <kusu0806@gmail.com>
2016-10-11 17:27:13 +0900

    Change to use React.Component, React.PureComponent

--- src/component/Item.js
+++ src/component/Item.js
@@ -1,29 +1,39 @@
 /* @flow */
-import React from 'react'
+// noinspection JSUnresolvedVariable
+import React, { PureComponent } from 'react'
+import type { ReactElementType } from '../definition/types'
 
-const Item = (props: {
-  userProfileImageUrl: string,
-  url: string,
-  title: string,
-  updatedAdInWords: string,
-  stockCount: number,
-}) => (
-  <tr>
-    <td width="100">
-      <img src={props.userProfileImageUrl} alt="アイコン" width="80" />
-    </td>
-    <td width="300">
-      <a href={props.url} target="_blank" rel="noopener noreferrer">
-        {props.title}
-      </a>
-    </td>
-    <td width="100">
-      {props.updatedAdInWords}前
-    </td>
-    <td width="100">
-      {props.stockCount}ストック
-    </td>
-  </tr>
-)
+class Item extends PureComponent {
+  props: {
+    userProfileImageUrl: string,
+    url: string,
+    title: string,
+    updatedAdInWords: string,
+    stockCount: number,
+  }
+
+  render(): ReactElementType {
+    const { userProfileImageUrl, url, title, updatedAdInWords, stockCount } = this.props
+
+    return (
+      <tr>
+        <td width="100">
+          <img src={userProfileImageUrl} alt="アイコン" width="80" />
+        </td>
+        <td width="300">
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </td>
+        <td width="100">
+          {updatedAdInWords}前
+        </td>
+        <td width="100">
+          {stockCount}ストック
+        </td>
+      </tr>
+    )
+  }
+}
 
 export default Item
--- src/component/ItemList.js
+++ src/component/ItemList.js
@@ -1,38 +1,47 @@
 /* @flow */
-import React from 'react'
+// noinspection JSUnresolvedVariable
+import React, { Component } from 'react'
 import Item from './Item'
-import type { ItemType } from '../definition/types'
+import type { ItemType, ReactElementType } from '../definition/types'
 
-const ItemList = (props: {
-  items: ItemType[],
-  loading: boolean,
-}) => (
-  <div>
-    {
-      props.loading ?
-        <font size="1">
-          Loading...
-        </font>
-        :
-        ''
-    }
-    <table>
-      <tbody>
+class ItemList extends Component {
+  props: {
+    items: ItemType[],
+    loading: boolean,
+  }
+
+  render(): ReactElementType {
+    const { items, loading } = this.props
+
+    return (
+      <div>
         {
-          props.items.map((item: ItemType) => (
-            <Item
-              key={item.uuid}
-              userProfileImageUrl={item.user.profile_image_url}
-              title={item.title}
-              url={item.url}
-              updatedAdInWords={item.updated_at_in_words}
-              stockCount={item.stock_count}
-            />
-          ))
+          loading ?
+            <font size="1">
+              Loading...
+            </font>
+            :
+            ''
         }
-      </tbody>
-    </table>
-  </div>
-)
+        <table>
+          <tbody>
+            {
+              items.map((item: ItemType) => (
+                <Item
+                  key={item.uuid}
+                  userProfileImageUrl={item.user.profile_image_url}
+                  title={item.title}
+                  url={item.url}
+                  updatedAdInWords={item.updated_at_in_words}
+                  stockCount={item.stock_count}
+                />
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
 
 export default ItemList
